371dde4dea810b2fd9a149690e25fde564367121
Mo <Kan10@kan10.home>
2020-12-14 17:33:02 +0100

    Ajout calcul

--- app/src/scenes/CorrectChange/CorrectChange.jsx
+++ app/src/scenes/CorrectChange/CorrectChange.jsx
@@ -8,17 +8,18 @@ function CorrectChange() {
   const { register, handleSubmit } = useForm();
 
   const onSubmit = (data) => {
-    alert("I give you " + data.money + "€.");
+    const nb = 20 - data.money;
+    alert("I give you " + nb + "€.");
   }
 
   return (
     <div className="CorrectChange">
       <br/><br/><br/><br/>
-      <label>You have to pay ... €.</label>
+      <label>You have to pay 20€.</label>
       <br/><br/><br/>
         <form onSubmit = {handleSubmit(onSubmit)}>
           <label>How much you give ?</label><br/><br/>
-          <input type= "number" name= "money" ref= {register} placeholder= "Enter money" /><br/>
+          <input type= "number" step= "0.01" name= "money" ref= {register} placeholder= "Enter money" /><br/>
           <input type= "submit" value= "Pay"/>
           <input type= "reset" value= "Clear"/>
         </form>
